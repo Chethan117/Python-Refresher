2d28021af92d66821f8bb688d62a29687629ae64
Chethan117 <31811714+Chethan117@users.noreply.github.com>
2024-10-10 12:44:12 +0100

    Delete Regex.py

--- Regex.py
+++
@@ -1,3 +0,0 @@
-In one of the topic-level changes you mentioned, I’ve noticed cases with and without spaces. Is there anything specific we should be aware of regarding this?
-
-I’ve validated the columns driving the metrics. Please find the attached sheets for review, and let me know if any additional data points are required to map them accordingly. Below are the business topics and situation descriptions, along with distinct scenarios in both the old and new data. However, our focus is on the highlighted areas—please advise if there's anything else I should be aware of.
