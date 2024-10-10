1cf8efe291ee797e6794b1a663cb9734f7781c39
Chethan117 <31811714+Chethan117@users.noreply.github.com>
2024-10-10 12:44:37 +0100

    Delete Sample_regex.py

--- Sample_regex.py
+++
@@ -1,5 +0,0 @@
-Prev Percentage Reverse Amount: The total amount reversed as a percentage of the previous transaction or debit on the account.
-
-Prev Percentage Reverse Count: The number of times a reversal was applied, expressed as a percentage of previous transaction counts.
-
-Prev Percentage Reverse on Credit Line: The percentage of the credit line impacted by the reversal of previous transactions, indicating how much of the available credit was adjusted due to the reversal
